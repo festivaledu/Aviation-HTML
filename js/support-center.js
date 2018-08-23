c2d2f06a40627ff02fede704badb8753a70b2497
Väinämö Łūmikērø <vainamo@git.festival.ml>
2018-08-23 13:42:54 +0200

    Added Modals, Flags, Input Descriptions and Input Help

--- js/support-center.js
+++ js/support-center.js
@@ -25,42 +25,96 @@ if (params.some(p => p.k === "q")) {
     }
 }
 
-document.querySelectorAll(".faq-card[data-q]").forEach(el => {
-    const q = el.getAttribute("data-q");
+if (document.querySelectorAll(".faq-card[data-q]")) {
+    document.querySelectorAll(".faq-card[data-q]").forEach(el => {
+        const q = el.getAttribute("data-q");
 
-    document.querySelector(`.faq-card[data-q="${q}"] .faq-card-content`).innerHTML += `<a href="${getURLForQuery(el.getAttribute("data-q"))}" class="permalink">Permalink</a>`;
-});
-
-document.querySelector(".sc-menu.contents .heading").addEventListener("click", ev => {
-    ev.target.parentElement.classList.toggle("expanded");
-});
-
-document.querySelectorAll(".sc-menu.contents a:not(.selected)").forEach(el => {
-    el.addEventListener("click", ev => {
-        ev.preventDefault();
-        ev.stopPropagation();
-
-        if (el.hasAttribute("data-q-link") && el.getAttribute("data-q-link")) {
-            const q = el.getAttribute("data-q-link");
-
-            window.location.assign(getURLForQuery(q));
-        }
+        document.querySelector(`.faq-card[data-q="${q}"] .faq-card-content`).innerHTML += `<a href="${getURLForQuery(el.getAttribute("data-q"))}" class="permalink">Permalink</a>`;
     });
-});
+}
 
-document.querySelectorAll(".faq-card-header").forEach(el => {
-    el.addEventListener("click", ev => {
+if (document.querySelector(".sc-menu.contents .heading")) {
+    document.querySelector(".sc-menu.contents .heading").addEventListener("click", ev => {
         ev.target.parentElement.classList.toggle("expanded");
-
-        if (document.querySelector(".sc-menu.contents a.selected")) {
-            document.querySelector(".sc-menu.contents a.selected").classList.remove("selected");
-        }
-
-        if (ev.target.parentElement.classList.contains("expanded")) {
-            document.querySelector(`[data-q-link="${ev.target.parentElement.getAttribute("data-q")}"]`).classList.add("selected");
-        }
     });
-});
+}
+
+if (document.querySelectorAll(".sc-menu.contents a:not(.selected)")) {
+    document.querySelectorAll(".sc-menu.contents a:not(.selected)").forEach(el => {
+        el.addEventListener("click", ev => {
+            ev.preventDefault();
+            ev.stopPropagation();
+
+            if (el.hasAttribute("data-q-link") && el.getAttribute("data-q-link")) {
+                const q = el.getAttribute("data-q-link");
+
+                window.location.assign(getURLForQuery(q));
+            }
+        });
+    });
+}
+
+if (document.querySelectorAll(".faq-card-header")) {
+    document.querySelectorAll(".faq-card-header").forEach(el => {
+        el.addEventListener("click", ev => {
+            ev.target.parentElement.classList.toggle("expanded");
+
+            if (document.querySelector(".sc-menu.contents a.selected")) {
+                document.querySelector(".sc-menu.contents a.selected").classList.remove("selected");
+            }
+
+            if (ev.target.parentElement.classList.contains("expanded")) {
+                document.querySelector(`[data-q-link="${ev.target.parentElement.getAttribute("data-q")}"]`).classList.add("selected");
+            }
+        });
+    });
+}
+
+if (document.querySelectorAll(".modal-trigger")) {
+    document.querySelectorAll(".modal-trigger").forEach(el => {
+        el.addEventListener("click", ev => {
+            ev.preventDefault();
+
+            if (!el.hasAttribute("data-modal-text")) {
+                return;
+            }
+
+            if (document.querySelector(".modal")) {
+                if (!el.hasAttribute("data-modal-type") && document.getElementById("modal-text")) {
+                    document.querySelector(".modal").classList.remove("error", "info", "question", "success", "warning");
+                } else if (el.hasAttribute("data-modal-title")) {
+                    document.querySelector(".modal").classList.add(el.getAttribute("data-modal-type"));
+                }
+
+                if (!el.hasAttribute("data-modal-title") && document.getElementById("modal-text")) {
+                    document.getElementById("modal-title").hidden = true;
+                } else if (el.hasAttribute("data-modal-title")) {
+                    document.getElementById("modal-title").hidden = false;
+                    document.getElementById("modal-title").innerText = el.getAttribute("data-modal-title");
+                }
+            
+                document.getElementById("modal-title-bar").hidden = !el.hasAttribute("data-modal-type") && !el.hasAttribute("data-modal-title");
+            }
+            
+            const modalText = el.getAttribute("data-modal-text");
+
+            if (!document.getElementById("modal-text")) {
+                alert(modalText);
+                return;
+            }
+
+            document.getElementById("modal-text").innerText = modalText;
+            document.body.classList.add("modal-open");
+        });
+    });
+}
+
+if (document.getElementById("close-modal")) {
+    document.getElementById("close-modal").addEventListener("click", ev => {
+        ev.preventDefault();
+        document.body.classList.remove("modal-open");
+    });
+}
 
 function getURLForQuery(query) {
     if (params.some(p => p.k === "q")) {
