77e1280781c49904f74622e71388aec032189a5e
SniperGER <janikschmidt@icloud.com>
2018-08-29 23:36:20 +0200

    Commented my files in /js

--- js/custom-footer.js
+++ js/custom-footer.js
@@ -1,3 +1,15 @@
+/**
+ * custom-footer.js
+ * FESTIVAL Aviation
+ * 
+ * Create a footer that contains random 
+ * text every time it's displayed
+ * 
+ * @author Janik Schmidt (jani.schmidt@ostfalia.de)
+ * @version 1.0
+ */
+
+// Store symbols found in Segoe MDL2 Assets
 let footers = [
 	"\uE822\uE81F",
 	"\uE724",
@@ -8,8 +20,10 @@ let footers = [
 	"\uE81F\uEC5A\uEC81\uEC81"
 ];
 
+// Create a reference to to footer DOM element
 let customFooter = document.querySelector("footer p.custom-footer span.weird-text");
 
+// If the footer exists, insert a random string from the array above
 if (customFooter) {
 	customFooter.innerHTML = footers[Math.floor(Math.random() * footers.length)];
 }
